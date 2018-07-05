24d1e51541cfc0c75a137f1390760e5846b3153f
ione88 <Alekseev_ivan@mail.ru>
2018-07-05 16:19:58 +0300

    post -> put metod
    Signed-off-by: ione88 <Alekseev_ivan@mail.ru>

--- src/main/java/com/ione88/myServer/repository/DnsProductRepository.java
+++ src/main/java/com/ione88/myServer/repository/DnsProductRepository.java
@@ -4,9 +4,13 @@ import com.ione88.myServer.entity.DnsProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 @Repository("DnsProductRepository")
 public interface DnsProductRepository extends JpaRepository<DnsProduct, Long> {
+    List<DnsProduct> findByNameContaining(String name);
+    List<DnsProduct> findByDescriptionContaining(String description);
     DnsProduct findByCode(Integer code);
 }
--- src/main/java/com/ione88/myServer/repository/YandexNewsRepository.java
+++ src/main/java/com/ione88/myServer/repository/YandexNewsRepository.java
@@ -3,9 +3,13 @@ package com.ione88.myServer.repository;
 import com.ione88.myServer.entity.YandexNews;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface YandexNewsRepository extends JpaRepository<YandexNews, Long> {
+    List<YandexNews> findByTitleContaining(String title);
+
     YandexNews findByTitle(String title);
 }
